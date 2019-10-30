d3bd21478bb3415ebf211dcd8b53234939a148c8
Shadowlocker <sivanfarnoush@yahoo.com>
2019-10-30 15:26:24 -0700

    add feed cards

--- app/screens/Feed.js
+++ app/screens/Feed.js
@@ -1,14 +1,147 @@
 import React, { Component } from 'react';
-import { Container } from 'native-base';
+import { Container, Content, Card, CardItem, Body, Text, Left, Right, Icon, Thumbnail, Button } from 'native-base';
 import styles from './styles.js';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, Image, FlatList } from 'react-native';
+
 
 export default class Feed extends Component {
     render() {
         return (
-            <Container style={styles.centered}>
-                <Text> EMPTY FEED SCREEN</Text>
-            </Container>
+	<Container>
+        <Content>
+          <Card>
+            <CardItem>
+              <Left>
+               <Thumbnail source={require('./assets/dog.png')} />
+                <Body>
+                 <Text>Foodie</Text>
+                  <Text note>Location CSUN</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+          <Image source = {require('./assets/howlin.png')} style={{height: 200, width: null, flex: 1}} />
+            </CardItem>
+            <CardItem>
+              <Left>
+               
+					 <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                 
+                
+              </Left>
+              <Body>
+                
+                <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+        
+          <Card>
+            <CardItem>
+              <Left>
+               <Thumbnail source={require('./assets/dog.png')} />
+                <Body>
+                 <Text>Foodie</Text>
+                  <Text note>Location CSUN</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+          <Image source = {require('./assets/howlin.png')} style={{height: 200, width: null, flex: 1}} />
+            </CardItem>
+            <CardItem>
+              <Left>
+               
+					 <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                 
+                
+              </Left>
+              <Body>
+                
+                <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+       
+          <Card>
+            <CardItem>
+              <Left>
+               <Thumbnail source={require('./assets/dog.png')} />
+                <Body>
+                 <Text>Foodie</Text>
+                  <Text note>Location CSUN</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+          <Image source = {require('./assets/howlin.png')} style={{height: 200, width: null, flex: 1}} />
+            </CardItem>
+            <CardItem>
+              <Left>
+               
+					 <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                 
+                
+              </Left>
+              <Body>
+                
+                <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+       
+          <Card>
+            <CardItem>
+              <Left>
+               <Thumbnail source={require('./assets/dog.png')} />
+                <Body>
+                 <Text>Foodie</Text>
+                  <Text note>Location CSUN</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+          <Image source = {require('./assets/howlin.png')} style={{height: 200, width: null, flex: 1}} />
+            </CardItem>
+            <CardItem>
+              <Left>
+               
+					 <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                 
+                
+              </Left>
+              <Body>
+                
+                <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
         )
     }
 }
