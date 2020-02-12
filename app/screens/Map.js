5e56a6cb0bf8fe6b943d0afc03afaaec3fb6537c
Aaron Tiania <aarontiania@gmail.com>
2020-02-12 14:13:11 -0800

    New branch to work on search bar with autocomplete functionality

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -27,6 +27,7 @@ class Map extends React.Component {
       longitudeDelta: 0.0121,
       error: null,
       nearbyRestaurants: [],
+      autocompleteResults: [],
       isLoaded: false,
       zoomLevel: 16
     };
@@ -116,6 +117,26 @@ class Map extends React.Component {
       return err;
     })
   }
+  autocompleteRestaurants = () => {
+    axios.get('https://api.yelp.com/v3/autocomplete', {
+        headers: { 'Authorization': 'Bearer 6wdE42fE4oWYKFvwpLn-FmGqaWQpmyjeAHQ2_jWwnuNqRB7-cSAkHcdOvxf4gK-3Xw3QDmGhHBv93U1e0yIsqjauRsKyW0fnbGE7VVBRbyLlSfSnbuSrbWP2karAXXYx' },
+        params: {
+            text: "in",
+            latitude: this.state.origLat,
+            longitude: this.state.origLong,
+            categories: "restaurants",
+            limit: 20
+        }
+    }).then((res) => {
+        this.setState({
+            autocompleteResults: res.data,
+            isLoaded: true
+        });
+    }).catch((err) => {
+        console.log("Yelp request via Axios failed: " + err);
+        return err;
+    })
+}
 
   render() {
     if (!this.state.isLoaded) {
