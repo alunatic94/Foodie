3fd64b8de3a189276a367ae6bea41df79955199b
Alberto <Alberto.luna.642@my.csun.edu>
2020-02-14 12:55:57 -0800

    connected to DB increments and decrements

--- app/components/LikeButton.js
+++ app/components/LikeButton.js
@@ -3,42 +3,75 @@ import { View, Text, StyleSheet} from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import { Icon, Button} from 'native-base';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import { db } from "../database/Database.js";
+import { db, firebase } from "../database/Database.js";
 import {User} from "../database/User.js";
 import { withNavigation, ScrollView } from "react-navigation";
 import Comment from "../components/Comment.js";
+import styles from '../screens/styles.js';
+
 
 class LikeButton extends React.Component {
+  componentDidMount() {
+    
+    //const statRef = db.collection('posts').doc('yZtajMCp8XKB8DwVjoIA').collection('likes').doc('ObRxoiLtx2g6gg6JWlKb');
+    //like=db.collection('posts').doc('yZtajMCp8XKB8DwVjoIA',count);
+    //numberOfLikes=db.collection('posts').doc()
+    
+    
+    
+   
 
- 
+    //const batch = db.batch();
+    //batch.set(likeRef, { title: 'Total Likes' });
+    //batch.set(statRef, { likecount: increment }, {merge: true});
+    //batch.commit();
+
+  // Update read count
+  //likeRef.update({ count: increment });
+  //likeRef.update({ reads: decrement });
+
+
+  //const statRef = db.collection('post').doc(`${Math.random()}`);
+    
+  }
 
   constructor(props){
 
     super(props);
     this.state = {
-      like: " ",
+      like: "0",
       updated: false,
       hearto: false,
+
+      likesArray: [],
+      buttonTextColor: '#0065ff',
+      user: User.dummyUser 
 	  
     };
 
   }
 
-
+ 
   updateLikes = () => {
+    likeRef = db.collection('posts').doc('yZtajMCp8XKB8DwVjoIA');
+    decrement = firebase.firestore.FieldValue.increment(-1);
+     increment = firebase.firestore.FieldValue.increment(1);
 
     if(!this.state.updated) {
       this.setState((prevState, props) => {
+        likeRef.update({ count: increment });
         return {
           like: prevState.like + 1,
           updated: true
 	
         };
+        
       });
 
     } else {
 
       this.setState((prevState, props) => {
+        likeRef.update({ count: decrement });
         return {
           like: prevState.like - 1,
           updated: false
@@ -52,17 +85,18 @@ class LikeButton extends React.Component {
 
   render(){
 	  
-	  const { hearts, heart, updated, firez, fire } = this.state;
+    const { hearts, heart, updated, firez, fire } = this.state;
+    
 
     return(
     <View style= { color= 'white'}>
 	   <Button transparent>
-	    <AntDesign name={hearts ? "heart" : "heart"} color={updated ? 'red' : 'rgb(237, 237, 237)'} size={30} onPress={this.updateLikes}/>
-        <Text>Likes {this.state.like}</Text>
+     <Text>Likes:{this.state.like}</Text>
+	    <AntDesign name={hearts ? "heart" : "heart"} color={updated ? 'red' : 'rgb(237, 237, 237)'} size={30} onPress={this.updateLikes}/> 
 	   </Button>
 	  </View>  
     );
   }
 }
 
-export default withNavigation(LikeButton);
+export default LikeButton;
