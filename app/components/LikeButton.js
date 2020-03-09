bdb70a7c345480f91c68356e7a702108fd240dcd
Aaron Tiania <aarontiania@gmail.com>
2020-03-09 01:14:29 -0700

    Updated Like Page
    Now correctly shows username, first last name, and profile picture

--- app/components/LikeButton.js
+++ app/components/LikeButton.js
@@ -1,56 +1,60 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
-import { Icon, Button, Container} from 'native-base';
+import { Icon, Button, Container } from 'native-base';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { db, firebase } from "../database/Database.js";
 import { User } from "../database/User.js";
 import { withNavigation, ScrollView } from "react-navigation";
 import Comment from "../components/Comment.js";
 import styles from '../screens/styles.js';
-import MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import LikePage from '../screens/LikePage.js';
 //import { ref } from '@hapi/joi';
 
 
 class LikeButton extends React.Component {
-  ref=db.collection('posts').doc(this.props.postID);
-  liketoinsert=db.collection('posts').doc(this.props.postID).collection('Likeby');
+  ref = db.collection('posts').doc(this.props.postID);
+  liketoinsert = db.collection('posts').doc(this.props.postID).collection('Likeby');
   componentDidMount() {
 
     this.ref.get().then((doc) => {
       ref = doc.data();
       this.setState({
-          like: ref.likes,
-          updated: false,
-          hearto: false,
-          liked:false,
-          user:User.dummyUser,
-          likesArray:[]
+        like: ref.likes,
+        updated: false,
+        hearto: false,
+        liked: false,
+        user: User.dummyUser,
+        likesArray: [],
+        postID: this.props.ID
       })
-  })
-}
+    });
 
-  constructor(props) {
-
-    super(props);
-    
-    this.state = {
-      like:'',
-      updated: false,
-      hearto: false,
-      likesArray: [],
-      liked:false,
-      //buttonTextColor: '#0065ff',
-      user: User.dummyUser 
-	  
-    };
     
 
   }
 
-  add =  () => {
-     
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      like: '',
+      updated: false,
+      hearto: false,
+      likesArray: [],
+      liked: false,
+      //buttonTextColor: '#0065ff',
+      user: User.dummyUser
+
+    };
+
+
+  }
+
+  add = () => {
+
     let likeData = {
       userID: User.getCurrentUserID()
 
@@ -61,20 +65,20 @@ class LikeButton extends React.Component {
   updateLikes = () => {
     likeRef = db.collection('posts').doc(this.props.postID);
     decrement = firebase.firestore.FieldValue.increment(-1);
-     increment = firebase.firestore.FieldValue.increment(1);
-    
+    increment = firebase.firestore.FieldValue.increment(1);
 
-    if(!this.state.updated) {
-      this.setState({liked:true});
+
+    if (!this.state.updated) {
+      this.setState({ liked: true });
       this.add();
       this.setState((prevState, props) => {
         likeRef.update({ likes: increment });
         return {
           like: prevState.like + 1,
           updated: true,
-          hearto:true,
-          liked:true
-	
+          hearto: true,
+          liked: true
+
         };
 
       });
@@ -87,29 +91,31 @@ class LikeButton extends React.Component {
           like: prevState.like - 1,
           updated: false,
           hearto: false,
-          liked:false
+          liked: false
         };
       });
 
     }
   }
 
-  render(){
-	  
-    const { hearts, heart,updated, firez, fire } = this.state;
-  
-    return(
-    <View style= { color= 'white'}>
+  render() {
 
-      <Button transparent onPress={() => this.props.navigation.navigate('LikePage',{postID: this.props.postID})}>
-      <Text>Likes:{this.state.like}</Text>
-      </Button>
+    const { hearts, heart, updated, firez, fire } = this.state;
 
-	   <Button transparent>
-     <MaterialCommunityIcons name={firez ? "fire" : "fire"} color={this.state.liked ? 'red' : 'rgb(237, 237, 237)'} size={45} onPress={this.updateLikes}/>
-	   </Button>
-    </View>  
-    
+    return (
+      <View style={color = 'white'}>
+
+        <Button transparent onPress={() => this.props.navigation.navigate('LikePage', {
+          postID: this.props.postID
+        })}>
+          <Text>Likes:{this.state.like}</Text>
+        </Button>
+
+        <Button transparent>
+          <MaterialCommunityIcons name={firez ? "fire" : "fire"} color={this.state.liked ? 'red' : 'rgb(237, 237, 237)'} size={45} onPress={this.updateLikes} />
+        </Button>
+      </View>
+
     );
   }
 }
--- app/screens/LikePage.js
+++ app/screens/LikePage.js
@@ -1,89 +1,163 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
-import { Icon, Button, Container, Header, Left, Footer, Right, ListItem, Thumbnail, Content, Item, Input} from 'native-base';
+import { Icon, Button, Container, Header, Left, Footer, Right, ListItem, Thumbnail, Content, Item, Input, Body } from 'native-base';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { db, firebase } from "../database/Database.js";
-import {User} from "../database/User.js";
+import { User } from "../database/User.js";
+import { ProfileDB } from "../database/ProfileDB.js"
 import { withNavigation, ScrollView } from "react-navigation";
 //import Comment from "../components/Comment.js";
 import styles from '../screens/styles.js';
-import MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import LikeButton from '../components/LikeButton.js';
 //import { ref } from '@hapi/joi';
 
 class LikePage extends Component {
   //liketoinsert=db.collection('posts').doc(this.props.navigation.getParam('postID', 'Qe1PUrFY32K8EYL9UYqW')).collection('Likeby');
 
+  posts = db.collection("posts");
+  users = db.collection("users");
+  docArray = []
+
   constructor(props) {
     super(props);
     this.state = {
-      likesArray: [],
-      user: User.dummyUser     
+      userID: this.props.navigation.getParam('userID', User.getCurrentUserID()),
+      postID: '',
+      likeUserID: [],
+      likeUsername: [],
+      isLoaded: false,
+      profileLoaded: false,
+      namesLoaded: false,
+      user: User.dummyUser
     };
   }
 
-  componentDidMount () {
-    //this.getall();
-    
+  async componentDidMount() {
+    var arr = [];
+    test = db.collection('posts').doc(this.props.navigation.state.params.postID).collection('Likeby').get().then(snapshot => {
+      snapshot.forEach(doc => {
+        arr.push(doc.data().userID)
+      }),
+        this.setState({
+          isLoaded: true,
+          likeUserID: arr
+        })
+    }).catch(err => {
+      console.log("Error getting Likeby")
+    })
+    var profileDB = new ProfileDB(this.state.userID);
+    await profileDB.getProfile().then((profile) => {
+      this.setState({
+        currentProfile: profile,
+        profileLoaded: true
+      })
+    });
+    var docArray = []
+    await this.state.likeUserID.map(user => {
+      users.doc(user).get().then(doc => {
+        var data = doc.data()
+        docArray.push({ name: data.username, profilePic: data.profileImage, first: data.first, last: data.last })
+        console.log(docArray)
+        this.setState({
+          likeUsername: docArray
+        })
+      }).catch(err => {
+        console.log("Error getting username");
+      })
+      this.setState({ namesLoaded: true })
+    })
   }
 
-  getall = () =>{
-    
-    const allLikes=this.liketoinsert
-    .get()
-    .then(snapshot => {
-      let existingLikes = [];
-      snapshot.forEach(doc => {
-        existingLikes.push(doc.data());
-      });
-      this.setState({likesArray: existingLikes});
-    })
-    
+
+  getall = () => {
+
+    const allLikes = this.liketoinsert
+      .get()
+      .then(snapshot => {
+        let existingLikes = [];
+        snapshot.forEach(doc => {
+          existingLikes.push(doc.data());
+        });
+        this.setState({ likesArray: existingLikes });
+      })
+
 
   };
-  
 
 
 
-    render() {
-        return (
-          <Container>        
-            <Header>
-              <Left>
-                <Button
-                  iconLeft
-                  light
-                  onPress={() => {
-                    this.props.navigation.goBack();
-                  }}
-                >
-                  <Icon name="arrow-back" />
-                </Button>
-              </Left>
-              <Text> Users that Liked this Photo</Text>
-            </Header>
-            <ScrollView>
-            
-            </ScrollView>
-           
-                        
-              <Footer>
-              <Container style={styles.commentsFooter}>
-                <ListItem avatar >
-                  <Left>
-                    <Thumbnail small source={{uri: this.state.user.profileImage}} />
-                  </Left>
-                </ListItem>            
-                  <Content>            
-            
-                    </Content>            
-                </Container>            
-              </Footer>
-          
-          </Container>
-        );
-      }
+
+  render() {
+    if (this.state.isLoaded && this.state.profileLoaded && this.state.namesLoaded) {
+      return (
+        <Container>
+          <Header>
+            <Left>
+              <Button
+                iconLeft
+                light
+                onPress={() => {
+                  this.props.navigation.goBack();
+                }}
+              >
+                <Icon name="arrow-back" />
+              </Button>
+            </Left>
+            <Text> Users that Liked this Photo</Text>
+          </Header>
+          <Content style={{marginTop: 10}}>
+            {
+              this.state.likeUsername.map(name => {
+                return (
+                  <TouchableOpacity style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "left",
+                    marginLeft: 20
+                  }}>
+                    <Image
+                        style={{ width: 70, height: 70, borderRadius: 70 /2, borderWidth: 2, borderColor: "beige", left: 0 }}
+                        source={{ uri: name.profilePic }}
+                    />    
+                    <Text style={{
+                      color: "#000",
+                      fontFamily: "Raleway Bold",
+                      fontSize: 23,
+                      marginLeft: 20
+                    }}>{name.name + "\n"}<Text style={styles.subheading}>{name.first + " " + name.last}</Text></Text>
+                  </TouchableOpacity>
+                )
+              })
+            }
+          </Content>
+
+
+          <Footer>
+            <Container style={styles.commentsFooter}>
+              <ListItem avatar >
+                <Left>
+                  <Thumbnail small source={{ uri: this.state.currentProfile.profileImage }} />
+                </Left>
+              </ListItem>
+              <Content>
+
+              </Content>
+            </Container>
+          </Footer>
+
+        </Container >
+      );
+    } else {
+      return (
+        <Container>
+          <Text>Loading...</Text>
+        </Container>
+      );
     }
-    
-    export default withNavigation(LikePage);
+
+  }
+}
+
+export default withNavigation(LikePage);
