1f24271fe73d04d1be3d77e689970207ed7c4a14
Victor Pineda <pinedavictor095@gmail.com>
2020-03-28 21:25:48 -0700

    comment structure

---
+++ app/components/comments/Comment.js
@@ -0,0 +1,64 @@
+
+import React, { Component } from 'react';
+import { Container, Content, Card, CardItem, List, ListItem, Body, Text, Left, Right, Icon, Thumbnail, Button, Header, Footer, View } from 'native-base';
+import styles from '../screens/styles.js';
+import {User} from "../database/User.js"
+import SubComment from '..//components/SubComment.js'
+
+const tempImage = require('../screens/assets/dog.png');
+
+class Comment extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: User.dummyUser,
+      subComments: [' ', ' ']
+    }
+  }
+
+  componentWillMount() {
+    this.loadUser();
+ }
+
+ loadUser = () => {
+     User.getExisting(this.props.userID).then((loadedUser) => {
+         this.setState({
+             user: loadedUser
+         })
+     })
+     .catch((err) => {
+         console.log(err + ":" + "Could not load user [id = " + this.props.userID + "] for comment");
+     })
+ }
+
+  handleReplyComment = () => {
+    // Prompt user for input
+    // Comment IDs?
+    console.log('Reply commment')
+  }
+
+    render(){
+        return(
+          <List>
+            <ListItem noBorder avatar>
+              <Left>
+                <Thumbnail source={{uri: this.state.user.profileImage}} style={styles.circleSmall}  />
+              </Left>
+              <Body style={{display: 'flex', flexDirection: 'row'}}>
+                <Text>{this.state.user.username}</Text>
+                <Text>{this.props.body}</Text>
+              </Body>
+              <Right>
+                <Text note>{this.props.time}</Text>
+              </Right>
+            </ListItem>
+            <Text note
+                style={{paddingLeft: 60}}
+                onPress={this.handleReplyComment}
+                >Reply</Text>
+                {this.state.subComments.map((commentID) => (<SubComment/>))}
+          </List>          
+        );
+    }
+}
+export default Comment;
