b2632506f41ad24af6f6afcfe099fad4329eccae
Aaron Tiania <aarontiania@gmail.com>
2020-04-01 20:13:10 -0700

    Base functionality completed. Need to add to router and change Feed.js to FriendFeed.js before merge

--- app/screens/Feed.js
+++ app/screens/Feed.js
@@ -1,71 +1,138 @@
 import React, { Component } from 'react';
-import { Container, Content } from 'native-base';
+import { Container, Content, View, Text } from 'native-base';
 import FeedCard from '../components/FeedCard.js';
 import { ScrollView } from 'react-native';
 import { db } from '../database/Database.js';
 import ScreenHeader from '../components/common/ScreenHeader.js'
+import { User } from "../database/User.js"
+import styles from './styles.js';
+import FriendPlates from '../components/Profile/FriendPlates.js';
 
 export default class Feed extends Component {
 
   posts = db.collection('posts');
+  friends = db.collection('friends');
+  users = db.collection('users');
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
+      userID: this.props.navigation.getParam('userID', User.getCurrentUserID()),
+      isLoaded: false,
+      plateID: [],
       posts: []
     }
   }
 
-  componentDidMount(){
+  async componentDidMount() {
     this.getAll();
-    const query = this.posts
-    const listener = query.onSnapshot(querySnapshot => {
-      this.getAll();
-    }, err => {
-      console.log("Encountered error: ${err}");
-    });
-  }
-
-  componentWillUnmount(){
-    const unMountListener = this.posts.onSnapshot(() => {});
-  }
-
-  getAll(){
-    const allPosts = this.posts
-      .orderBy('timestamp', 'desc')
-      .get()
-      .then(snapshot => {
-        let existingPosts = [];
-        snapshot.forEach(doc => {
-          post = {
-            postID: doc.id,
-            data: doc.data()
-          }
-          existingPosts.push(post);
-        });
-        this.setState({posts: existingPosts});
-      })
-      .catch(err => {
-        console.log("Error getting existing posts " + err);
-      });
-  }
-
-    render() {
-        return (
-	<Container>    
     
-        <ScreenHeader navigation = {this.props.navigation} title="Feed">
+  }
+
+  async getPlateID(user) {
+    let plates = []
+    const ret = await this.users
+      .doc(user)
+      .get()
+      .then(res => {
+        let data = res.data().plates
+        plates = res.data().plates
+        var temp = this.state.plateID.concat(data)
+        this.setState({ plateID: temp })
+      })
+    return plates
+  }
+
+  async getPost(id) {
+    const ret = await this.posts
+      .doc(id)
+      .get()
+      .then(doc => {
+        post = {
+          postID: doc.id,
+          data: doc.data()
+        }
+        if (post.data != undefined) {
+          var temp = this.state.posts
+          temp.push(post)
+          temp.sort(this.compare)
+          this.setState({ posts: temp })
+        }
+      })
+    return ret
+  }
+
+  compare(a, b) {
+    const postone = a.data.timestamp
+    const posttwo = b.data.timestamp
+
+
+    let comparison = 0
+    if (postone > posttwo) {
+      comparison = 1
+    } else if (postone < posttwo) {
+      comparison = -1
+    }
+    return comparison * -1
+  }
+
+  componentWillUnmount() {
+    const unMountListener = this.posts.onSnapshot(() => { });
+  }
+
+  async getAll() {
+    var friendslist = []
+    await this.friends
+      .doc(this.state.userID)
+      .get()
+      .then(friends => {
+        let data = friends.data()
+        Object.keys(data).forEach(userID => {
+          if (data[userID]) {
+            friendslist.push(userID)
+          }
+        })
+      })
+    var friendsplates = []
+    await friendslist.forEach(item => {
+      this.getPlateID(item).then(ret => {
+        ret.forEach(id => {
+          this.getPost(id).then(() => {
+            this.setState({ isLoaded: true })
+          })
+        })
+      })
+    })
+  }
+
+  render() {
+    if (!this.state.isLoaded) {
+      return (
+        <Container>
+          <View style={styles.centeredTest}>
+            <Text>Loading!</Text>
+          </View>
+        </Container>
+      )
+    }
+    return (
+      <Container>
+
+        <ScreenHeader navigation={this.props.navigation} title="Feed">
         </ScreenHeader>
 
         <Content>
-        {/* TODO: Dynamically load post ids from collection to create Feedcard for each one */}
-         {/* <FeedCard postID="Qe1PUrFY32K8EYL9UYqW"/>
-         <FeedCard postID="aDpWMJ1UfX7U2rdbvXtR"/> */}         
-          <ScrollView/>
-          {this.state.posts.map((post) => <FeedCard key={post.postID} postID={post.postID}/>)}        
-         <ScrollView/>       
+          {/* TODO: Dynamically load post ids from collection to create Feedcard for each one */}
+          {/* <FeedCard postID="Qe1PUrFY32K8EYL9UYqW"/>
+         <FeedCard postID="aDpWMJ1UfX7U2rdbvXtR"/> */}
+          <ScrollView />
+          {
+            this.state.posts.map((post) => <FeedCard key={post.postID} postID={post.postID} />)
+          }
+          <ScrollView />
         </Content>
-      </Container> 
-        )
-    }
+      </Container>
+
+    )
+  }
 }
